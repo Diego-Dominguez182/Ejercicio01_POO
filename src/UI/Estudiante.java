03f6168146ff46089bdf9f7c7512eee7dcc25669
Gabriel_Morales <toni.morales.gmg@gmail.com>
2023-08-28 17:09:40 -0600

    Initial commit

---
+++ src/UI/Estudiante.java
@@ -0,0 +1,16 @@
+package UI;
+
+public class Estudiante {
+    //Atributos
+    public String nombre;
+    public String correo;
+    public String matricula;
+    public String telefono;
+    public String semestre;
+
+    //Constructor
+    Estudiante(String nombre, String correo){
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+}
